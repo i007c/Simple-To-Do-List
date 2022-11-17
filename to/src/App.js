53c1fa290a3229d08a22ab71a15848178ce79d75
Dontalion <danialhaddad50@gmail.com>
2022-11-17 11:38:46 +0330

    compelete stracture of project

---
+++ to/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+    return(
+        <div className='todo-app'>
+            <h1>
+                To Do App
+            </h1>
+        </div>
+    )
+}
+
+export default App;
